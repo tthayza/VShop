e4dd511b46b47dabe83ad7b4d229b97050dbd814
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2024-05-31 11:13:54 -0300

    feat: Add views index and remove item from cart

--- VShopWeb/Models/CartHeaderViewModel.cs
+++ VShopWeb/Models/CartHeaderViewModel.cs
@@ -5,5 +5,5 @@ public class CartHeaderViewModel
     public int Id { get; set; }
     public string UserId { get; set; } = string.Empty;
     public string CouponCode {  get; set; } = string.Empty;
-    public double TotalAmount { get; set; } = 0.00d;
+    public decimal TotalAmount { get; set; } = 0.00m;
 }
